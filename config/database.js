c9b042b91ac580876a04c5a375fa1878b0617ff0
isuru9798 <isr.ruwantha@gmail.com>
2022-06-22 01:10:40 +0530

    change mongo url

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const { env } = require("process");
 
-mongoose.connect(env.MONGO_URL)
+const MONGO_URL = 'mongodb+srv://dbIsuru:dbisuru@myanmar.3cqc2dc.mongodb.net/taxi_db';
+
+mongoose.connect(MONGO_URL)
     .then(() => {
         console.log("successfully connected to database")
     })
